b2239ff04708c641a15ee8de8759b59e8284e8b4
Yaron <ygreif@gmail.com>
2016-08-06 19:17:33 -0700

    Python2->Python3: xrange. SimulationParameters. 120 flake8 limit

--- simulator.py
+++ simulator.py
@@ -1,23 +1,41 @@
 import numpy
 
 
+class SimulationParameters(object):
+    def __init__(self, discount=.9, max_iterations=1000, tolerance=0.01):
+        self.discount = discount
+        self.max_iterations = max_iterations
+        self.tolerance = tolerance
+
+
 class Simulator(object):
-    def __init__(self, state_space, action_space, discount):
+    def __init__(self, state_space, action_space, parameters=SimulationParameters()):
         self.state_space = state_space
         self.action_space = action_space
-        self.discount = discount
-        self.max_iterations = 1000
-        self.tolerance = 0.01
+        self.parameters = parameters
+
+    @property
+    def discount(self):
+        return self.parameters.discount
+
+    @property
+    def max_iterations(self):
+        return self.parameters.max_iterations
+
+    @property
+    def tolerance(self):
+        return self.parameters.tolerance
 
     def find_best_policy_for_state(self, state, value_func):
         best_action = None
         best_payoff = float('-inf')
         for a, payoff in self.action_space.get_actions(state):
+            acc_payoff = payoff
             for next_state, prob in self.action_space.transition_probabilities(a, state):
-                payoff += value_func[next_state] * prob * self.discount
-            if payoff > best_payoff:
+                acc_payoff += value_func[next_state] * prob * self.discount
+            if acc_payoff > best_payoff:
                 best_action = a
-                best_payoff = payoff
+                best_payoff = acc_payoff
         return best_action, best_payoff
 
     def iterate(self, value_func):
@@ -33,9 +51,9 @@ class Simulator(object):
 
     def find_best_policy(self):
         value_func = numpy.ndarray(self.state_space.dim())
-        for i in range(0, self.max_iterations):
+        for i in range(self.max_iterations):
             next_value_func, next_policy_func = self.iterate(value_func)
-#            if numpy.amax(abs(value_func - next_value_func)) < self.tolerance:
- #               break
+            if numpy.amax(abs(value_func - next_value_func)) < self.tolerance:
+                break
             value_func = next_value_func
         return next_value_func, next_policy_func
